5995df52574910b95a93a91c6bbe6ee700de9003
Aurora <tomek-kubel2@wp.pl>
2021-01-18 20:07:57 +0100

    add basic batching

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::{sync::atomic::{AtomicBool, Ordering}, time::{Duration, Instant}};
 
 use midi::{
     axiom::{AxiomAirController, AxiomMessage, Button},
@@ -27,39 +27,53 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             std::fs::write("procs.rs", dump_procs_sigs(&services))?;
             println!("Done.");
         }
-        Some("run") => {
-            let mut krpc = KrpcConnection::connect("127.0.0.1:50000", "midi")?;
-            println!("TCP connected.");
-            let (controller, recv) = AxiomAirController::new()?;
-            println!("Midi controller connected.");
-
-            while !STOP.load(Ordering::SeqCst) {
-                for e in recv.try_iter() {
-                    match e {
-                        AxiomMessage::Sustain(Button::Pressed) => {
-                            let is_paused: bool = krpc.is_paused()?.unwrap();
-                            krpc.pause(!is_paused)?.unwrap();
-                        }
-                        AxiomMessage::Stop(Button::Pressed) => {
-                            let mut vessel = krpc.get_active_vessel()?.unwrap();
-                            let mut control = vessel.get_control()?.unwrap();
-                            let throttle = control.get_throttle()?.unwrap();
-                            println!("Throttle: {}", throttle);
-                        }
-                        AxiomMessage::Knob(1, v) => {
-                            let mut vessel = krpc.get_active_vessel()?.unwrap();
-                            let mut control = vessel.get_control()?.unwrap();
-                            control.set_throttle(v as f32 / 127.0)?.unwrap();
-                        }
-                        _ => (),
-                    }
-                }
-            }
-
-            controller.close()?;
-        }
+        Some("run") => run()?,
         _ => println!("Usage: midi [run|dump_services]"),
     }
 
+    Ok(())
+}
+
+fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let mut krpc = KrpcConnection::connect("127.0.0.1:50000", "midi")?;
+    println!("TCP connected.");
+    let (controller, recv) = AxiomAirController::new()?;
+    println!("Midi controller connected.");
+
+    // TODO: make struct for this pattern?
+    let mut current_throttle = 0;
+    let mut set_throttle = 0;
+
+    let mut timer = Instant::now();
+    while !STOP.load(Ordering::SeqCst) {
+        for e in recv.try_iter() {
+            match e {
+                AxiomMessage::Sustain(Button::Pressed) => {
+                    let is_paused: bool = krpc.is_paused()?.unwrap();
+                    krpc.pause(!is_paused)?.unwrap();
+                }
+                AxiomMessage::Stop(Button::Pressed) => {
+                    let mut vessel = krpc.get_active_vessel()?.unwrap();
+                    let mut control = vessel.get_control()?.unwrap();
+                    let throttle = control.get_throttle()?.unwrap();
+                    println!("Throttle: {}", throttle);
+                }
+                AxiomMessage::Knob(1, v) => set_throttle = v,
+                _ => (),
+            }
+        }
+        // Batch network calls
+        if timer.elapsed() > Duration::from_secs(1) / 60 {
+            if set_throttle != current_throttle {
+                let mut vessel = krpc.get_active_vessel()?.unwrap();
+                let mut control = vessel.get_control()?.unwrap();
+                control.set_throttle(set_throttle as f32 / 127.0)?.unwrap();
+                current_throttle = set_throttle;
+            }
+            timer = Instant::now();
+        }
+    }
+
+    controller.close()?;
     Ok(())
 }
